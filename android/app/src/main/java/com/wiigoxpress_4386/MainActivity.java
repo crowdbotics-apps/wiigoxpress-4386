4c83797e6b99dba2583c63b17fe548e6997d3125
Crowdbotics <team@crowdbotics.com>
2019-06-08 07:51:39 +0000

    initial commit

---
+++ android/app/src/main/java/com/wiigoxpress_4386/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wiigoxpress_4386;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "wiigoxpress_4386";
+    }
+}
